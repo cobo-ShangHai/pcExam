0d29bc6a412fef80cb994c9333eb15c8e5378d60
jsliu <withfreewind@gmail.com>
2019-04-02 16:01:26 +0800

    切换到产品模式

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -26,12 +26,12 @@ export class AuthGuardService implements CanActivate {
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any> | boolean {
 
     // 开发环境 localhost:4321/index.html#/exam/details;eid=eb5VYBLuhV8  ; 考试eid
-     const _token = 'kMy4HqApfME'; // 测试用的token test01
-     return this.cs.isTokenEffective(_token);
+    //  const _token = 'kMy4HqApfME'; // 测试用的token test01
+    //  return this.cs.isTokenEffective(_token);
 
 
     // 正式环境
-    //  return true;
+    return true;
 
 
 
